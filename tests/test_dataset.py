1c1629febaec127743ef0107cbe9fa24e7fd45a7
evamy <antriksh.don@gmail.com>
2018-04-13 09:03:45 -0500

    tests for dataset

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -3,15 +3,30 @@
 # @author: Antriksh Agarwal
 # Version 0: 4/13/2018
 
+import time
+
 
 def test_dataset():
 
-	from DataSet import DataSet
+    from DataSet import DataSet
 
-	start = time.time()
+    start = time.time()
     d = DataSet()
     print time.time() - start
     for row in d.iterate():
         print row
         break
 
+
+def test_data_to_records():
+    from DataSet import DataSet
+
+    d = DataSet()
+    r = None
+    for row in d.iterate():
+        r = row
+        break
+
+    for row in d.to_records():
+        assert r[1] == row[1].split(';')
+        break
